d40bbb4cacbbceeff6511292044098732041d5ca
menglin <menglinlin1203@gmail.com>
2017-10-17 16:27:27 +0800

    n

--- js/jump.js
+++ js/jump.js
@@ -1,10 +1,17 @@
+$(window).load(function(){
+	$('#loading').hide();
+	});
+
 $(function (){
     $(window).load(function(){
         $(window).scroll(function(){
     var $this = $(this);
     var $thisTop=$this.scrollTop();
+    var _x = $('#intro').height();
+    var _y = $('#guide').height();
+    var _z = _x + _y;
                         
-    if($thisTop < 50){
+    if($thisTop < _z){
         $('#goTop').hide ();
     }
     else{
