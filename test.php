5d280bd2b4e6c1e96b5cdb84305b09785f73dc76
menglin <menglinlin1203@gmail.com>
2018-01-13 11:11:37 +0800

    test

---
+++ test.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+  <title>計算機實作</title>
+</head>
+
+<body>
+  <form action="show.php" method="post">
+    <input type="text" name="a" placeholder="車價">
+    <br>
+    <input type="text" name="b" placeholder="時間">
+    <br>
+    <button type="reset">重設</button>
+    <button type="submit">送出</button>
+
+  </form>
+</body>
+
+</html>
