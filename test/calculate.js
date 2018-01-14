b29d25a77fd9af35728fd99eedd8a69ae10d8086
menglin <menglinlin1203@gmail.com>
2018-01-14 14:29:37 +0800

    GO

--- test/calculate.js
+++ test/calculate.js
@@ -12,10 +12,10 @@ window.onload = function () {
 
     n2 = document.getElementById("money").value; //input的數字放到n裡
     b2 = new Big(n2);
-    if (isNaN(this.value.valueOf()) || this.value == "" || this.value < 0 || this.value.indexOf(" ") != -1) { //判斷數字不是負的或是空白或是文字
-
+    if (isNaN(this.value.valueOf()) || this.value == "" || this.value < 0 || this.value.indexOf(" ") != -1 || isNaN(n1.valueOf()) || n1 == "" || n1 < 0 || n1.indexOf(" ") != -1) { //判斷數字不是負的或是空白或是文字
       return false;
-    } else if (!isNaN(this.value) && this.value != "") { //判斷用
+    }
+    if (!isNaN(this.value) && this.value != "") { //判斷用
       var a1 = this.value;
       c1 = new Big(a1);
       var d1 = c1 * b1;
@@ -27,8 +27,7 @@ window.onload = function () {
 
     n1 = document.getElementById("rate").value;
     b1 = new Big(n1);
-    if (isNaN(this.value.valueOf()) || this.value == "" || this.value < 0 || this.value.indexOf(" ") != -1) {
-
+    if (isNaN(this.value.valueOf()) || this.value == "" || this.value < 0 || this.value.indexOf(" ") != -1 || isNaN(n2.valueOf()) || n2 == "" || n2 < 0 || n2.indexOf(" ") != -1) {
       return false;
     }
     if (!isNaN(this.value) && this.value != "") {
@@ -36,8 +35,23 @@ window.onload = function () {
       c2 = new Big(a2);
       var d2 = c2 * b2;
       document.getElementById("answer").innerHTML = d2.toFixed(1) + "萬元";
-    } else {
-      document.getElementById("answer").innerHTML = "空格應輸入數字！";
     }
   };
+
+  document.getElementById("rate").onkeydown = function () { //打字時執行
+
+    n1 = document.getElementById("rate").value;
+    if (isNaN(n1.valueOf()) || n1 == "" || n1 < 0 || n1.indexOf(" ") != -1) {
+      document.getElementById("answer").innerHTML = "請輸入數字";
+    }
+  };
+
+  document.getElementById("money").onkeydown = function () { //打字時執行
+
+    n2 = document.getElementById("money").value;
+    if (isNaN(n2.valueOf()) || n2 == "" || n2 < 0 || n2.indexOf(" ") != -1) {
+      document.getElementById("answer").innerHTML = "請輸入數字";
+    }
+  };
+
 }
