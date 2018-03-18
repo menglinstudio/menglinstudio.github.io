517dcfbc9f543b0c0a50a4dc4da0e6ad0c08430d
menglin <menglinlin1203@gmail.com>
2018-03-18 15:49:31 +0800

    website beta2.0 testing

--- js/motion.js
+++ js/motion.js
@@ -1,44 +1,38 @@
-$(window).load(function(){
-	$('#wait').hide();
-	});
+$(window).load(function () {
+  $('#wait').hide();
+});
 
-$(function (){
-    $(window).load(function(){
-        $(window).scroll(function(){
-    var $this = $(this);
-    var $thisTop=$this.scrollTop();
-    var $thisWidth=jQuery(window).width();
-    var _x = $('#intro1').height();
-                        
-    if($thisTop < _x){
-        $('#floatnav').hide();
-    }
-    else if($thisWidth > 900){
-        $('#floatnav').hide();   
-    }
-    else{
-        $('#floatnav').show ();
-　　　}                
-　　})
-　});
+$(function () {
+  $(window).load(function () {
+    $(window).scroll(function () {
+      var $this = $(this);
+      var $thisTop = $this.scrollTop();
+
+      if ($thisTop > 100) {
+        $('#top-button').show();
+      } else {
+        $('#top-button').hide();　　　
+      }　　
+    })　
+  });
 });
 
 
 
-function filter(param){
-    if(param == 2){
-        $(".portButton").hide();
-        $(".filterOther").show();
-        
-    }else if(param == 3){
-        $(".portButton").hide();
-        $(".filterApp").show();
-        
-    }else if(param == 4){
-        $(".portButton").hide();
-        $(".filterWeb").show();
-        
-    }else {
-        $(".portButton").show();  
-    }
-    }
+function filter(param) {
+  if (param == 2) {
+    $(".portButton").hide();
+    $(".filterOther").show();
+
+  } else if (param == 3) {
+    $(".portButton").hide();
+    $(".filterApp").show();
+
+  } else if (param == 4) {
+    $(".portButton").hide();
+    $(".filterWeb").show();
+
+  } else {
+    $(".portButton").show();
+  }
+}
